c270c3a0aa67c928b8e5c0c402fe579911f29b5c
Afif Akromi <massachusetts48@gmail.com>
2021-10-02 12:31:10 +0700

    login register auth

---
+++ save_member.php
@@ -0,0 +1,35 @@
+<?php
+	//starting the session
+	session_start();
+
+	//including the database connection
+	require_once 'conn.php';
+	
+	if(ISSET($_POST['register'])){
+		// Setting variables
+		$email = $_POST['email'];
+		$name = $_POST['name'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$is_admin = 0;
+		
+		// Insertion Query
+		$query = "INSERT INTO `member` (email, name, username, password, is_admin) VALUES(:email, :name, :username, :password, :is_admin)";
+		$stmt = $conn->prepare($query);
+		$stmt->bindParam(':email', $email);
+		$stmt->bindParam(':name', $name);
+		$stmt->bindParam(':username', $username);
+		$stmt->bindParam(':password', $password);
+		$stmt->bindParam(':is_admin', $is_admin);
+		
+		// Check if the execution of query is success
+		if($stmt->execute()){
+			//setting a 'success' session to save our insertion success message.
+			$_SESSION['success'] = "Successfully created an account";
+
+			//redirecting to the index.php 
+			header('location: index.php');
+		}
+
+	}
+?>
