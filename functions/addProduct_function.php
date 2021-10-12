7a9d0ddfc2114c992e7d4c4a40b720682197fbb9
M Fadli Gunardi <68526207+mfadl1@users.noreply.github.com>
2021-10-13 00:57:52 +0700

    ganti harga di db jadi int

--- functions/addProduct_function.php
+++ functions/addProduct_function.php
@@ -2,30 +2,19 @@
   session_start();
       $connection = new PDO("sqlite:"."../db/dorayaki.db");
       if (isset($_POST["submit1"])){
-        $count = 0;
-        $query = $connection->query("SELECT count(*) FROM Produk WHERE nama = '$_POST[productName]';");
-        $count = $query->fetch(PDO::FETCH_ASSOC);
-        echo $count;
-        if ($count > 0){
+        $nRow = $connection->query("SELECT count(*) FROM Produk WHERE nama = '$_POST[productName]'")->fetchColumn(); 
+        if ($nRow > 0){
           $_SESSION['gagal'] = "This Product is Already Exist!!";
         }
         else{
           try {
-            $connection->query("INSERT INTO `Produk` (nama, deskripsi, harga, stok, gambar) VALUES('$_POST[productName]', 
-          '$_POST[description]', '$_POST[price]', '$_POST[stock]' , '$_POST[link_photo]')");
+            $connection->query("INSERT INTO `Produk` (nama, deskripsi, stok, gambar, harga) VALUES('$_POST[productName]', 
+          '$_POST[description]', '$_POST[stock]' , '$_POST[link_photo]', '$_POST[price]')");
           $_SESSION['sukses'] = "Insert Product Succed";
           } catch(Exception $e) {
               die('connection_unsuccessful: ' . $e->getMessage());
           }
-         
-        ?>
-        <script type="text/javascript">
-          document.getElementById('error').style.display = "none";
-          document.getElementById('succes').style.display = "block";
-        </script>
-      <?php
-
         }
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
+      header('Location: ' . $_SERVER['HTTP_REFERER']);
       }
       ?>
